97c8c4487ac1d13e99179adb93a9826bfc000d26
cedricmarville <marville.cedric972@gmail.com>
2022-11-10 17:24:17 +0100

    manque tags

--- src/Components/Tags/index.jsx
+++ src/Components/Tags/index.jsx
@@ -8,20 +8,18 @@ import apartmentProfiles from '../../Utils/Logements/logements.json'
 function Tags() {
   // Déclaration de la constante Id avec useParams
   const { id } = useParams()
-
   // Récupération de la liste des attributs du logement avec .find
   const Logement = apartmentProfiles.find((logement) => logement.id === id)
-
   // Déclaration de la constante pour récupérer la note du logment
   const wordOfTags = Logement.tags
-
   // Parcourir le tableau et afficher toutes les valeurs
-  for (var i = 0; i < wordOfTags.length; i++) {
-    const wordOfTagsName = `
-    ${wordOfTags[i]}`
+  // for (var i = 0; i < wordOfTags.length; i++) {
+  //   const wordOfTagsName = `
+  //   ${wordOfTags[i]}`
 
-    console.log(wordOfTagsName)
-  }
+  //   console.log(wordOfTags[i])
+  //   console.log(wordOfTagsName)
+  // }
   return <p className="tagsLogement">{wordOfTags}</p>
 }
 
